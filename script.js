9f9b534076e8ab32968bcbc016e5ec0441582e89
joeneedum <joeneedum@gmail.com>
2022-03-03 17:18:19 -0500

    commit

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
-let apiQuotes = [];
+// let apiQuotes = [];
 
-// Get Quotes from API
-async function getQuotes() {
-    const apiUrl = 'https://type.fit/api/quotes';
-    try {
-        const response = await fetch(apiUrl);
-        apiQuotes = await response.json();
-        console.log(apiQuotes);
-    } catch (error) {
-        // Catch Error Here
-    }
+// Show New Quote
+function NewQuote() {
+    
+    // Pick a random quote from apiQuotes array
+    const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)]
+    console.log(quote);
 }
 
+// Get Quotes from API
+// async function getQuotes() {
+//     const apiUrl = 'https://type.fit/api/quotes';
+//     try {
+//         const response = await fetch(apiUrl);
+//         apiQuotes = await response.json();
+//         NewQuote()
+//     } catch (error) {
+//         // Catch Error Here
+//     }
+// }
+
 // On Load
-getQuotes();
+// getQuotes();
+NewQuote()
+
